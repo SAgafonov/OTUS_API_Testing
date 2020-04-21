2ab197a42700c05de112ca4b744f64897ae93954
Sergey <emigi@destiny.games>
2020-04-21 11:02:18 +0300

    add API tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ import requests
 def pytest_addoption(parser):
     parser.addoption(
         "--url",
-        required=True,
+        default="https://ya.ru",
+        required=False,
         help="Request URL"
     )
 
@@ -13,7 +14,15 @@ def pytest_addoption(parser):
         "--method",
         default="get",
         choices=["get", "post"],
+        required=False,
         help="Method to execute"
+    )
+
+    parser.addoption(
+        "--status_code",
+        default="200",
+        required=False,
+        help="Response status code"
     )
 
 
@@ -29,3 +38,37 @@ def get_method(request):
         return requests.get
     elif m == "post":
         return requests.post
+
+
+@pytest.fixture
+def get_status(request):
+    return request.config.getoption("--status_code")
+
+
+@pytest.fixture
+def url_dog_api_list_of_breeds():
+    return "https://dog.ceo/api/breeds"
+
+
+@pytest.fixture
+def url_dog_api_one_breed():
+    return "https://dog.ceo/api/breed"
+
+
+@pytest.fixture
+def url_for_breweries():
+    return "https://api.openbrewerydb.org/breweries"
+
+
+@pytest.fixture
+def url_for_jsonplaceholder():
+    return "https://jsonplaceholder.typicode.com/posts"
+
+
+@pytest.fixture
+def body_for_jsonplaceholder():
+    return {
+      "title": "foo",
+      "body": "bar",
+      "userId": 1
+    }
--- tests/test_dog_api.py
+++ tests/test_dog_api.py
@@ -1,17 +1,40 @@
+import cerberus
 import pytest
-# import requests
+import requests
 
 
-@pytest.mark.parametrize("sub_url, code", [("/breeds/list/all", 200),
-                                           ("/breeds/image/random", 200),
-                                           ("/breeds/image/random/3", 200),
-                                           ("/breed/hound/list", 200)])
-def test_random_breed(get_url, sub_url, code, get_method):
-    response = get_method(get_url + sub_url)
+@pytest.mark.parametrize("sub_url, code", [("/list/all", 200),
+                                           ("/image/random", 200),
+                                           ("/image/random/3", 200)])
+def test_check_breeds_urls(url_dog_api_list_of_breeds, sub_url, code):
+    response = requests.get(url_dog_api_list_of_breeds + sub_url)
     assert response.status_code == code
 
 
-# @pytest.mark.parametrize("dog, code", [("affenpinscher", 200), ("basenjd", 404)])
-# def test_breeds_list(dog, code, get_method):
-#     response = get_method("https://dog.ceo/api/breed/" + dog + "/images/random")
-#     assert response.status_code == code
+@pytest.mark.parametrize("sub_url, code", [("/hound/images", 200),
+                                           ("/hound/images/random", 200),
+                                           ("/hound/images/random/3", 200),
+                                           ("/hound/list", 200),
+                                           ("/hound/afghan/images", 200),
+                                           ("/hound/afghan/images/random", 200),
+                                           ("/hound/afghan/images/random/3", 200)])
+def test_check_breed_urls(url_dog_api_one_breed, sub_url, code):
+    response = requests.get(url_dog_api_one_breed + sub_url)
+    assert response.status_code == code
+
+
+def test_list_all_breeds(url_dog_api_one_breed):
+    schema = {
+        "message": {"type": "list"},
+        "status": {"type": "string"}
+    }
+    v = cerberus.Validator()
+    response = requests.get(url_dog_api_one_breed + "/hound/images")
+    assert v.validate(response.json(), schema)
+
+
+@pytest.mark.parametrize("dog, code", [("/affenpinscher", 200), ("/basenjd", 404)])
+def test_breeds_list(url_dog_api_one_breed, dog, code):
+    response = requests.get(url_dog_api_one_breed + dog + "/images/random")
+    assert response.status_code == code
+
